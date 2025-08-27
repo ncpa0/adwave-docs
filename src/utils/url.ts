02b1a854555f62209a40c7eea2c4263cea85e710
ncpa0cpl <szymonb21@gmail.com>
2025-08-27 14:31:37 +0200

    feat: remove prefix from urls

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,8 +1,8 @@
 import path from "node:path";
 import process from "node:process";
 
-const BASE_URL = process.env.BASE_URL ?? "/docs/";
+// const BASE_URL = process.env.BASE_URL ?? "/docs/";
 
 export function url(...url: string[]) {
-  return path.join(BASE_URL, ...url);
+  return path.join(...url);
 }
